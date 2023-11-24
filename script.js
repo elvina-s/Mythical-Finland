268a7f755a203a027595961c1455f61d88c8cee1
elvina-s <rammnuschka@gmail.com>
2023-11-24 21:48:04 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,3 @@
+gsap.from("#mainScreenButton", {y: 50, opacity: 0, delay: 2.3, duration: 4, ease: "bounce"});
+gsap.from("#mainScreenHeading", {opacity: 0, delay: 0.5, duration: 1.5});
+gsap.from("#mainScreenSubHeading", {opacity: 0, delay: 1.5, duration: 1.5});
